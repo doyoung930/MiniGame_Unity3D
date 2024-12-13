9ff0c892202edd0c241fb5b02b6fee39060b4a49
doyoung930 <doyoung930@gmail.com>
2024-12-14 01:19:47 +0900

    collsion

---
+++ ObstacleGame/Assets/ObjectHit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("Bumped into a wall");
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
