196e8737299a9773d1d35c5a4b2e502756bd3d91
doyoung930 <doyoung930@gmail.com>
2024-12-15 04:42:28 +0900

    키 입력

---
+++ RocketGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust(){
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(0, 1, 0);
+        }
+        
+    }
+    void ProcessRotation(){
+        if(Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Pressed A");
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Pressed D");
+        }
+    }
+}
