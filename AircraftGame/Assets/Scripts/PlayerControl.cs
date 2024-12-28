f15beed57d53985b25865e8b2e94ab0708452053
doyoung930 <doyoung930@gmail.com>
2024-12-29 05:00:06 +0900

    인풋 시스템 추가

--- AircraftGame/Assets/Scripts/PlayerControl.cs
+++ AircraftGame/Assets/Scripts/PlayerControl.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
+    [SerializeField] InputAction movement;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    void OnEnable() {
+        movement.Enable();
+    }
+    void OnDisable() {
+        movement.Disable();    
+    } 
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalThrow = movement.ReadValue<Vector2>().x;
+        float verticalThrow = movement.ReadValue<Vector2>().y;
     }
 }
