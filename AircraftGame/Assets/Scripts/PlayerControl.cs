0cd27320fe5981e8a74825f6b4626863d889f860
doyoung930 <doyoung930@gmail.com>
2024-12-30 07:29:47 +0900

    파티클 추가 및 활성화 비활성화 코드 추가

--- AircraftGame/Assets/Scripts/PlayerControl.cs
+++ AircraftGame/Assets/Scripts/PlayerControl.cs
@@ -5,20 +5,81 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float controlSpeed = 20f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 6f;
+
+    [SerializeField] GameObject[] lasers;
+
+    [SerializeField] float positionPitchFactor = -3f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float positionYawFactor = 3f;
+    [SerializeField] float controlRollFactor = -20f;
+
+
+    float xThrow, yThrow;
+
 
     // Update is called once per frame
     void Update()
     {
-        float xThrwow = Input.GetAxis("Horizontal");
-        float yThrwow = Input.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+        ProcessFiring();
+    }
 
-        float xOffset = xThrwow * Time.deltaTime * controlSpeed;
-        float newXPos = transform.localPosition.x + xOffset;
+    void ProcessTranslation(){
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
-        float yOffset = yThrwow * Time.deltaTime * controlSpeed;
-        float newYPos = transform.localPosition.y + yOffset;
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
-        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+    void ProcessRotation(){
+        float pitchDueToPosition = transform.localPosition.y* positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+
+    void ProcessFiring()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            //Debug.Log("true");
+            ActiveLasers();
+        }
+        else{
+            //Debug.Log("false");
+            DeactivateLasers();
+        }
+    }
+
+    void ActiveLasers()
+    {
+        
+        foreach (GameObject laser in lasers){
+            laser.SetActive(true);
+        }
+    }
+    void DeactivateLasers()
+    {
+        foreach (GameObject laser in lasers){
+            laser.SetActive(false);
+        }
     }
 }
+
