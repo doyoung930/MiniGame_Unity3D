7aa64fabee65953d352898c278c99731bf87409b
doyoung930 <doyoung930@gmail.com>
2025-01-01 23:52:05 +0900

    UI에 score 표시

--- AircraftGame/Assets/Scripts/Enemy.cs
+++ AircraftGame/Assets/Scripts/Enemy.cs
@@ -6,8 +6,25 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        // 절대 Update에 쓰여선 안된다.
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
     void OnParticleCollision(GameObject other)
     {
+        PreocessHit();
+        KillEnemy();
+       
+    }
+
+    void PreocessHit(){
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+    void KillEnemy(){
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(gameObject);
