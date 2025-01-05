4e606671990cbbfca755f87478ed9b282d5edafd
doyoung930 <doyoung930@gmail.com>
2025-01-06 06:09:01 +0900

    타일 재구성 및 manager에 전달

--- TowerDefense/Assets/Tile/CoordinateLabeler.cs
+++ TowerDefense/Assets/Tile/CoordinateLabeler.cs
@@ -71,10 +71,11 @@ public class CoordinateLabeler : MonoBehaviour
         }
     }
 
-    void DisplayCoordinates() 
+    void DisplayCoordinates()
     {
-        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / 10);
-        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / 10);
+        if (gridManager == null) return;
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / gridManager.UnityGridSize);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / gridManager.UnityGridSize);
         label.text = coordinates.x + "," + coordinates.y;
     }
 
